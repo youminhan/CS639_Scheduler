80c5bb595cdec7f320d846eca5ad8c8df7a98f20
Youmin Han <yhan222@wisc.edu>
2020-04-19 17:00:32 -0500

    Updates

---
+++ precache-manifest.d257943f2c10919ae7d97b341df04352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c15248a2623f288a17ec78353f92fe",
+    "url": "/CS639_Scheduler/index.html"
+  },
+  {
+    "revision": "a2329c28aca29e3a1b5c",
+    "url": "/CS639_Scheduler/static/css/main.818aee9e.chunk.css"
+  },
+  {
+    "revision": "f557c22009507b2aba77",
+    "url": "/CS639_Scheduler/static/js/2.77ec8678.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/CS639_Scheduler/static/js/2.77ec8678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2329c28aca29e3a1b5c",
+    "url": "/CS639_Scheduler/static/js/main.d23b8255.chunk.js"
+  },
+  {
+    "revision": "f26f245a34eec0446217",
+    "url": "/CS639_Scheduler/static/js/runtime-main.034d98c3.js"
+  }
+]);
